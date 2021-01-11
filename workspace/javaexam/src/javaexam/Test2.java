56d01849758143357105fd783485018a583b7445
carami <carami@nate.com>
2021-01-11 15:36:53 +0900

    add exam

--- workspace/javaexam/src/javaexam/Test2.java
+++ workspace/javaexam/src/javaexam/Test2.java
@@ -1,10 +1,41 @@
 package javaexam;
+abstract class 새{
+	abstract public void 노래하다() ;
+}
+class 참새 extends 새{
+	public void 노래하다() {
+		System.out.println("짹짹");
+	}
+}
 
+class 오리 extends 새{
+	public void 노래하다() {
+		System.out.println("꽥꽥");
+	}
+}
+
+class 비둘기 extends 새{
+
+	@Override
+	public void 노래하다() {
+		System.out.println("구구");
+	}
+	
+	
+}
 public class Test2 {
-
+	public static 새 get새(int kind) {
+		if(kind == 1)
+			return new 오리();
+		else if( kind == 2)
+			return new 참새();
+		else if (kind == 3)
+			return new 비둘기();
+		return null;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		새 b =Test2.get새(Integer.parseInt(args[0]));
+		b.노래하다();
 	}
 
 }
